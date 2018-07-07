2471ea9aabda258e5eb68becaf5f605d3088f95e
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-07-07 05:32:02 -0400

    api called and table set for data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
 import React, { Component }from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Dialog.js';
+import IconButton from '@material-ui/core/IconButton';
+import MUIDataTable from "mui-datatables";
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+// columns per array
+//const columns = ["Name", "City", "Birth", "Phone"];
+const columns = [
+  {
+   name: "Id",
+   options: {
+    filter: true,
+    sort: true,
+   }
+  },      
+  {
+   name: "Documents",
+   options: {
+    filter: true,
+    sort: false,
+   }
+  },
+  {
+   name: "Score",
+   options: {
+    filter: true,
+    sort: false,
+   }
+  },  
+  {
+   name: "Name",
+   options: {
+    filter: true,
+    sort: false,
+   }
+  },
+ ];
+ 
+// add array to parse string the data output because for postman 
+const data = [
+["54345597-3cfc-4958-ae06-17e9072af392", "detectedLanguages", "0.1", "English"],
+["54345597-3cfc-4958-ae06-17e9072af392", "sentiment", " 0.90802371501922607", ""],
+["54345597-3cfc-4958-ae06-17e9072af392", "enitites", " 0.90802371501922607", ""]
+
+];
+
+const options = {
+  filterType: 'checkbox',
+};
 
 
-var str = '{"languageDetection": {"documents": [{"id": "54345597-3cfc-4958-ae06-17e9072af392","detectedLanguages": [ {  "name": "English",  "iso6391Name": "en",  "score": 1.0  }  ]  }  ],  "errors": []  },"sentiment": {  "documents": [{  "id": "54345597-3cfc-4958-ae06-17e9072af392",    "score": 0.90802371501922607  }],  "errors": []},"entities": {  "documents": [  {  "id": "54345597-3cfc-4958-ae06-17e9072af392","entities": []  }],  "errors": []}}'
+var string = '{"languageDetection": {"documents": [{"id": "54345597-3cfc-4958-ae06-17e9072af392","detectedLanguages": [ {  "name": "English",  "iso6391Name": "en",  "score": 1.0  }  ]  }  ],  "errors": []  },"sentiment": {  "documents": [{  "id": "54345597-3cfc-4958-ae06-17e9072af392",    "score": 0.90802371501922607  }],  "errors": []},"entities": {  "documents": [  {  "id": "54345597-3cfc-4958-ae06-17e9072af392","entities": []  }],  "errors": []}}'
 try {
-  var obj = JSON.parse(str);
+  var obj = JSON.parse(string);
 console.log('languageDetection obj:', obj.languageDetection.documents[0])
 console.log('sentiment obj:', obj.sentiment.documents[0])
  console.log('entities obj:' , obj.entities.documents);
@@ -24,16 +77,35 @@ console.log(obj2);
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
+        <AppBar position="static">
+         <Toolbar>
+           <IconButton color="inherit" aria-label="Menu">
+             <MenuIcon />
+           </IconButton>
+           <Typography variant="title" color="inherit">
+             Title
+           </Typography>
+           <Button color="inherit">Login</Button>
+         </Toolbar>
+       </AppBar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{text}</h1>
+          <h1 className="App-title">Welcome to the Text Analysis API</h1>
         </header>
-        <p className="App-intro">
-        {str}  </p>
+        <p className="App-intro"> Only One Data Set (purchase)  </p>
+
+<MUIDataTable 
+  title={"Language Detection"} 
+  data={data} 
+  columns={columns} 
+  options={options} 
+/>
           <p>
         checkout the dev tools <code>source code</code> to see data.
         </p>
+        {text}
       </div>
     );
   }
--- src/Dialog.js
+++ src/Dialog.js
@@ -1,41 +1,63 @@
-import React, { Component,  Button  }from 'react';
+const https = require ('https');
+const event = ('');
+const options = { method: 'POST', url: 'https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/languages', body: { documents: [{ "text": event.text }] }, json: true, headers: { 'Ocp-Apim-Subscription-Key': process.env.MS_LOCALE_KEY } };
+console.log(options, 'this key has been found');
 
-import { Dialog } from 'react-toolbox/lib/dialog';
 
-class DialogTest extends React.Component {
-  constructor(props){
-    super(props);
-  }
+  // **********************************************
+  // *** Update or verify the following values. ***
+  // **********************************************
 
-  state = {
-    Dialog: Dialog,
-    active: false
+  // Replace the accessKey string value with your valid access key.
+  let accessKey = 'enter key here';
+
+  // Replace or verify the region.
+
+  // You must use the same region in your REST API call as you used to obtain your access keys.
+  // For example, if you obtained your access keys from the westus region, replace
+  // "westcentralus" in the URI below with "westus".
+
+  // NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+  // a free trial access key, you should not need to change this region.
+  let uri = 'westus.api.cognitive.microsoft.com';
+  let path = '/text/analytics/v2.0/keyPhrases';
+
+  let response_handler = function (response) {
+      let body = '';
+      response.on ('data', function (d) {
+          body += d;
+      });
+      response.on ('end', function () {
+          let body_ = JSON.parse (body);
+          let body__ = JSON.stringify (body_, null, '  ');
+          console.log (body__);
+      });
+      response.on ('error', function (e) {
+          console.log ('Error: ' + e.message);
+      });
   };
 
-  handleToggle = () => {
-    this.setState({active: !this.state.active});
+  let get_key_phrases = function (documents) {
+      let body = JSON.stringify (documents);
+
+      let request_params = {
+          method : 'POST',
+          hostname : uri,
+          path : path,
+          headers : {
+              'Ocp-Apim-Subscription-Key' : accessKey,
+          }
+      };
+
+      let req = https.request (request_params, response_handler);
+      req.write (body);
+      req.end ();
   }
 
-  actions = [
-    { label: "Cancel", onClick: this.handleToggle },
-    { label: "Save", onClick: this.handleToggle }
-  ];
+  let documents = { 'documents': [
+      { 'id': '1', 'language': 'en', 'text': 'I really enjoy the new XBox One S. It has a clean look, it has 4K/HDR resolution and it is affordable.' },
+      { 'id': '2', 'language': 'es', 'text': 'Si usted quiere comunicarse con Carlos, usted debe de llamarlo a su telefono movil. Carlos es muy responsable, pero necesita recibir una notificacion si hay algun problema.' },
+      { 'id': '3', 'language': 'en', 'text': 'The Grand Hotel is a new hotel in the center of Seattle. It earned 5 stars in my review, and has the classiest decor I\'ve ever seen.' }
+  ]};
 
-  render () {
-    return (
-      <div>
-        <Button label='Show my dialog' onClick={this.handleToggle} />
-        <Dialog
-          actions={this.actions}
-          active={this.state.active}
-          onEscKeyDown={this.handleToggle}
-          onOverlayClick={this.handleToggle}
-          title='My awesome dialog'
-        >
-          <p>Here you can add arbitrary content. Components like Pickers are using dialogs now.</p>
-        </Dialog>
-      </div>
-    );
-  }
-}
-export default Dialog;
+  get_key_phrases (documents);
