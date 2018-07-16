2192d9516c968fccad8ca6a199877c344a4f1994
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-07-16 16:06:50 -0400

    readding api

--- src/Title.js
+++ src/Title.js
@@ -61,7 +61,7 @@ class TodoApp extends React.Component{
     this.state = {
       data: []
     }
-    this.apiUrl = '//backend-api-2018.herokuapp.com/todos'
+    this.apiUrl = '//5b42da2463839a00144c015d.mockapi.io/aimee/todo'
   }
   
   // Lifecycle method
